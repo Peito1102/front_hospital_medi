ad6e84284b57f799ee4c96a7d9d2e7af8d31983c
Peito1102 <renzitiwin@gmail.com>
2025-10-07 17:38:27 -0500

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HospitalComponent } from './components/hospital/hospital.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
