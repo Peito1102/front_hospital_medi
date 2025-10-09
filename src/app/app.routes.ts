b7389339b83c6425d6587ef19be6d83fc57fbb70
Peito1102 <renzovasquezch@hotmail.com>
2025-10-09 03:43:37 -0500

    login diseño finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,24 +5,24 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component
 import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    // 🏠 Grupo de rutas con header + footer
-  {
-    path: '',
-    component: MainLayoutComponent,
-    children: [
-      { path: '', component: HospitalComponent }, // http://localhost:4200/
-      { path: 'home', component: HospitalComponent },
-    ],
-  },
 
   // 🔐 Grupo de rutas de autenticación (solo header)
   {
     path: '',
     component: AuthLayoutComponent,
     children: [
-      { path: 'login', component: LoginComponent },
+      { path: '', component: LoginComponent },
     ],
   },
+    // 🏠 Grupo de rutas con header + footer
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'home', component: HospitalComponent },
+    ],
+  },
+
 
   // 🚧 Redirección si la ruta no existe
   { path: '**', redirectTo: '' },
